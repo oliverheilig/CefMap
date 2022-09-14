57c8eae4ce04dc73704178995f7e3d70e9cd6a92
PersonalAccessToken <oliver.heilig@yahoo.de>
2022-09-14 08:31:32 +0200

    added geocoding

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mapPanel = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.locationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.locationTypeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.referencePositionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.roadAccessPositionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.formattedAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qualityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 189);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 156);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Find";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(695, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Karlsruhe Haid-und-Neu-Straße 15";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // mapPanel
+            // 
+            this.mapPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapPanel.Location = new System.Drawing.Point(0, 189);
+            this.mapPanel.Name = "mapPanel";
+            this.mapPanel.Size = new System.Drawing.Size(800, 261);
+            this.mapPanel.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.locationTypeDataGridViewTextBoxColumn,
+            this.referencePositionDataGridViewTextBoxColumn,
+            this.roadAccessPositionDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.formattedAddressDataGridViewTextBoxColumn,
+            this.qualityDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.locationBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // locationBindingSource
+            // 
+            this.locationBindingSource.DataSource = typeof(GeocodingClient.Model.Location);
+            // 
+            // locationTypeDataGridViewTextBoxColumn
+            // 
+            this.locationTypeDataGridViewTextBoxColumn.DataPropertyName = "LocationType";
+            this.locationTypeDataGridViewTextBoxColumn.HeaderText = "LocationType";
+            this.locationTypeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.locationTypeDataGridViewTextBoxColumn.Name = "locationTypeDataGridViewTextBoxColumn";
+            this.locationTypeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // referencePositionDataGridViewTextBoxColumn
+            // 
+            this.referencePositionDataGridViewTextBoxColumn.DataPropertyName = "ReferencePosition";
+            this.referencePositionDataGridViewTextBoxColumn.HeaderText = "ReferencePosition";
+            this.referencePositionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.referencePositionDataGridViewTextBoxColumn.Name = "referencePositionDataGridViewTextBoxColumn";
+            this.referencePositionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // roadAccessPositionDataGridViewTextBoxColumn
+            // 
+            this.roadAccessPositionDataGridViewTextBoxColumn.DataPropertyName = "RoadAccessPosition";
+            this.roadAccessPositionDataGridViewTextBoxColumn.HeaderText = "RoadAccessPosition";
+            this.roadAccessPositionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.roadAccessPositionDataGridViewTextBoxColumn.Name = "roadAccessPositionDataGridViewTextBoxColumn";
+            this.roadAccessPositionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // formattedAddressDataGridViewTextBoxColumn
+            // 
+            this.formattedAddressDataGridViewTextBoxColumn.DataPropertyName = "FormattedAddress";
+            this.formattedAddressDataGridViewTextBoxColumn.HeaderText = "FormattedAddress";
+            this.formattedAddressDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.formattedAddressDataGridViewTextBoxColumn.Name = "formattedAddressDataGridViewTextBoxColumn";
+            this.formattedAddressDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // qualityDataGridViewTextBoxColumn
+            // 
+            this.qualityDataGridViewTextBoxColumn.DataPropertyName = "Quality";
+            this.qualityDataGridViewTextBoxColumn.HeaderText = "Quality";
+            this.qualityDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.qualityDataGridViewTextBoxColumn.Name = "qualityDataGridViewTextBoxColumn";
+            this.qualityDataGridViewTextBoxColumn.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mapPanel);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locationBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel mapPanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationTypeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn referencePositionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn roadAccessPositionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn formattedAddressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qualityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource locationBindingSource;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,56 +2,100 @@
 using CefSharp.WinForms;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+using System.Text.Json;
+using GeocodingClient.Api;
+using GeocodingClient.Model;
 
 namespace CefMap
 {
     public partial class Form1 : Form
     {
+        private static readonly string apiKey = ""; // Get your free api key at https://developer.myptv.com/
+        private static LocationsApi locationsApi; 
+
         public Form1()
         {
             InitializeComponent();
 
             InitializeChromium();
+
+            if (string.IsNullOrEmpty(apiKey))
+                MessageBox.Show("You need an api key! Get your free key at https://developer.myptv.com/");
+
+            locationsApi = new LocationsApi(new GeocodingClient.Client.Configuration
+            {
+                ApiKey = new Dictionary<string, string>
+                {
+                    ["apiKey"] = apiKey
+                }
+            });
         }
 
-        private ChromiumWebBrowser chromeBrowser;
+        private ChromiumWebBrowser browser;
 
         public void InitializeChromium()
-        {           
+        {
             // Create a browser component
-            chromeBrowser = new ChromiumWebBrowser(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "Map.html"));
+            browser = new ChromiumWebBrowser(Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase,
+                $"Map.html?apiKey={apiKey}"));
+
             // Add it to the form and fill it to the form window.
-            this.Controls.Add(chromeBrowser);
-            chromeBrowser.Dock = DockStyle.Fill;
+            this.mapPanel.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
 
-            // Wait for the MainFrame to finish loading
-            chromeBrowser.FrameLoadEnd += (sender, args) =>
+            browser.JavascriptMessageReceived += OnReceiveNotification;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = locationsApi.SearchLocationsByText(textBox1.Text);
+
+            SetLocations(result.Locations);
+        }
+
+        private void OnReceiveNotification(object sender, JavascriptMessageReceivedEventArgs e)
+        {
+            if (e == null)
+                return;
+
+            dynamic message = e.Message;
+            switch (message.Event)
             {
-                // Wait for the MainFrame to finish loading
-                if (args.Frame.IsMain)
-                {
-                    double latitude = 8.4044;
-                    double longitude = 49.01405;
+                case "click":
+                    // need to warp longitude (outside -180/+180)
+                    var result = locationsApi.SearchLocationsByPosition(message.Lat, (message.Lng % 360 + 540) % 360 - 180);
 
-                    args.Frame.ExecuteJavaScriptAsync(FormattableString.Invariant(
-                        $@"setMarker({latitude}, {longitude}, 15, 80, 10000);"));
-                }
-            };
+                    // have to invoke it on the WinForms thread
+                    if(InvokeRequired) 
+                        Invoke(new Action(() => SetLocations(result.Locations)));
+                    break;
+            }
+        }
+
+        private void SetLocations(List<Location> locations)
+        {
+            dataGridView1.DataSource = locations;
+
+//            browser.ShowDevTools();
+
+            browser.GetMainFrame().ExecuteJavaScriptAsync(FormattableString.Invariant(
+                $@"setLocations('{JsonSerializer.Serialize(locations)}')"));
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                button1_Click(sender, null);
+            }
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            chromeBrowser.Dispose();
+            browser.Dispose();
             Cef.Shutdown();
         }
-     }
+    }
 }
